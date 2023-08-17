5d73cfba99dc76fb07a17bea3148945eaf967f69
Arthur Rodrigues <arthurodrigues4025@gmail.com>
2023-08-17 16:26:53 -0300

    Fazendo o 1 exercicio

---
+++ Aula 2 Prática/Exercicio 1 Aula Prática.py
@@ -0,0 +1,16 @@
+#Expressões algébricas
+
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+sa = s1 + ' ' + s2 + ' ' + s3
+sb = 10*(s1+ ' ')
+sc = s1 + ' '+2*(s2 + ' ') + ' ' + 3*(s3+' ')
+sd = (s1 + s2 + ' ')*6
+se = (s2*2 +s3 + ' ')*5
+
+print(sa)
+print(sb)
+print(sc)
+print(sd)
+print(se)
